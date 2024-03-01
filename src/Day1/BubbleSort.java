3ccd888df10843ce048a1248a354efdcb75b142b
consultadd-pratham <pratham.consultadd@gmail.com>
2024-03-01 11:33:54 -0500

    Day 1 completed

---
+++ src/Day1/BubbleSort.java
@@ -0,0 +1,31 @@
+package Day1;
+
+public class BubbleSort {
+    static void bubbleSort(int[] arr) {
+        int length = arr.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void displayArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        bubbleSort(arr);
+        displayArray(arr);
+    }
+}
