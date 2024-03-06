31553be67dcb2b5acfe4add92a7d108e63972f07
consultadd-pratham <pratham.consultadd@gmail.com>
2024-03-06 15:13:48 -0500

    Day 4 RotatingArray optimized

--- src/Day4/RotatingArray.java
+++ src/Day4/RotatingArray.java
@@ -2,29 +2,24 @@ package Day4;
 
 public class RotatingArray {
     public static void main(String[] args) {
-        int d = 3;
-        int[] arr = {-1, -2, -3, 4, 5, 6, 7};
-        int n = arr.length;
+        int[] arr = {-1, -2, -3, 4, 5, 6, 7, 9};
+        int d = 5;
+        reverseArray(arr, 0, arr.length - 1);
+        reverseArray(arr, 0, arr.length - 1 - d);
+        reverseArray(arr, arr.length - d, arr.length - 1);
 
-
-        for (int i = n / 2; i > 0; i++) {
-            reverseArray(arr, i, 0, n);
-        }
-
-        for (int j : arr) {
-            System.out.print(j + " ");
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 
-    static void reverseArray(int[] arr, int front, int end, int d) {
-
-        int i = 0;
-        while (i < arr.length / 2) {
-
-            int temp = arr[front + d];
-            arr[front] = arr[end - front - 1];
-            arr[end - front - 1] = temp;
-            i++;
+    static void reverseArray(int[] arr, int front, int end) {
+        while (front < end) {
+            int temp = arr[front];
+            arr[front] = arr[end];
+            arr[end] = temp;
+            front++;
+            end--;
         }
     }
 }
